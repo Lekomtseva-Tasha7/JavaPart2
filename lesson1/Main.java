e4a97f8219664232b04d20131dc48b8fe8210362
Lekomtseva-Tasha7 <100291835+Lekomtseva-Tasha7@users.noreply.github.com>
2022-06-17 15:42:53 +0500

    Add files via upload

--- lesson1/Main.java
+++ lesson1/Main.java
@@ -1,12 +1,21 @@
 package lesson_1;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите название команды:");
         Scanner scanner = new Scanner(System.in);
+
+        //Если падает при разделителе в числе ".", значит русская локаль (она работает с ","), можно поменять локаль.
+        System.out.println("Текущая локаль: " + scanner.locale());
+        scanner.useLocale(Locale.US);
+        System.out.println("Смена локали на US");
+        System.out.println("Обновленная локаль: " + scanner.locale());
+        //****************************
+
+        System.out.println("Введите название команды:");
         String teamName = scanner.nextLine();
 
         Team team1 = new Team(teamName);
